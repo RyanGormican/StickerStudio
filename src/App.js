759fd49d2fdff98e55de4f51aeaa6b8f663c2ae7
RyanGormican <ryan.gormican@gmail.com>
2023-11-20 01:40:46 -0600

    a
    a

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import React,{useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 import './App.css';
 
 function App() {
-  // Dynamic import of images from the "Backgrounds" and "Stickers" folders
   const backgroundContext = require.context('./Backgrounds', false, /\.(png|jpe?g|svg)$/);
   const stickerContext = require.context('./Stickers', false, /\.(png|jpe?g|svg)$/);
-
+  const [view,setView] = useState('stickers');
   const backgrounds = backgroundContext.keys().map(backgroundContext);
   const stickers = stickerContext.keys().map(stickerContext);
 
@@ -16,17 +15,18 @@ function App() {
     
       </div>
 
-      <div className="container-fluid" style={{ height: '15vh', backgroundColor: '#282c34', padding: '10px' }}>
-        <div className="row">
+       <div className="row"  style={{ backgroundColor: '#282c34'}}>
           <div className="col-md-6">
             <button className="btn btn-primary btn-block">Stickers</button>
           </div>
           <div className="col-md-6">
-            <button className="btn btn-secondary btn-block">Buttons</button>
+            <button className="btn btn-secondary btn-block">Backgrounds</button>
           </div>
         </div>
+      <div className="container-fluid" style={{ height: '15vh', backgroundColor: '#282c34', padding: '10px' }}>
+       
 
-
+      (view === 'stickers' && (
         <div className="row mt-3">
           {stickers.map((sticker, index) => (
             <div key={index} className="col-md-2">
@@ -34,8 +34,16 @@ function App() {
             </div>
           ))}
         </div>
-
-   
+        ))
+       (view === 'backgrounds' && (
+        <div className="row mt-3">
+          {backgrounds.map((background, index) => (
+            <div key={index} className="col-md-2">
+              <img src={background} alt={`Background ${index + 1}`} style={{ width: '100%' }} />
+            </div>
+          ))}
+        </div>
+        ))
       </div>
     </div>
   );
